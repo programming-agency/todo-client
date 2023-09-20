12d27560bcd48161020503d5530f9c3341f03457
HASHEBUR RAHMAN <mdhasheburrahman3@gmail.com>
2023-09-20 14:51:12 +0600

    :rocket: axios

--- src/Componemts/AddTodo.jsx
+++ src/Componemts/AddTodo.jsx
@@ -1,4 +1,4 @@
-import { Stack, Box, Paper, Typography, TextField, Button, Autocomplete, FormControl, InputLabel, Select, MenuItem } from "@mui/material"
+import { Stack, Box, Paper, Typography, TextField, Button,  FormControl, InputLabel, Select, MenuItem } from "@mui/material"
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { appAxios } from "../Axios";
@@ -33,16 +33,13 @@ export const AddTodo = () => {
             paddingX: {
                 lg: '100px'
             },
-
         }} >
-
             <Box sx={{
                 height: "550px",
                 backgroundColor: "white",
                 paddingX: {
                     lg: '20px',
                     xs: '2px'
-
                 },
                 paddingY: {
                     lg: '20px',
@@ -51,28 +48,28 @@ export const AddTodo = () => {
                     lg: '20px',
                     xs: '10px'
                 },
-
             }}>
                 <Typography
                     paddingY={"20px"}
-                    variant="h4"
-                >
+                    variant="h4">
                     New Tod
                 </Typography>
 
+                {/* from */}
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <Stack spacing={5}  >
                         <TextField
                             error={errors.label}
                             helperText={errors.label && "please enter your label be at minimum 6 characters maximum length 20 characters"}
                             fullWidth
+                            label='Label'
                             {...register("label",
                                 {
                                     required: true,
                                     minLength: 6,
                                     maxLength: 20
                                 })}
-                            label='Label' />
+                        />
 
                         <TextField
                             multiline
@@ -93,7 +90,6 @@ export const AddTodo = () => {
                             spacing={1}
                             display='flex'
                             alignItems='center'>
-
                             <FormControl fullWidth>
                                 <InputLabel
                                     id="demo-simple-select-label">Status</InputLabel>
@@ -120,7 +116,6 @@ export const AddTodo = () => {
                                 fullWidth />
                         </Stack>
                         <Button variant="contained" type="submit">Submit</Button>
-
                     </Stack>
                 </form>
             </Box>
--- src/Componemts/TodoList.jsx
+++ src/Componemts/TodoList.jsx
@@ -1,22 +1,20 @@
 
-import { Button, Stack, Box, Item, Card, Typography, CardContent, CardHeader, Avatar, IconButton, CardMedia, Switch, Grid, List, ListSubheader, ListItemButton, ListItemIcon, ListItemText, Collapse, Link } from '@mui/material';
+import { Stack, Card, Typography, CardHeader, Avatar, IconButton, Grid, List, ListSubheader, ListItemButton, ListItemIcon, ListItemText, Collapse, Link } from '@mui/material';
 import { useEffect, useState } from 'react';
 import { red } from '@mui/material/colors';
-import { ExpandLess, ExpandMore, StarBorder } from '@mui/icons-material';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
 import GridViewIcon from '@mui/icons-material/GridView';
 import { appAxios } from '../Axios';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 
-const label = { inputProps: { 'aria-label': 'Switch demo' } };
 
 export default function TodoList() {
 
     const [todos, setTodos] = useState([])
 
     useEffect(() => {
-
         const getAllTodo = async () => {
-
             const todos = await appAxios.get("/todo/todos")
             setTodos(todos.data);
         }
@@ -24,12 +22,11 @@ export default function TodoList() {
         getAllTodo();
     }, [])
 
-    const [open, setOpen] = useState(true);
+    const [open, setOpen] = useState(false);
 
     const handleClick = () => {
         setOpen(!open);
     };
-
 
     return (
         <Stack direction={'row'} spacing={1}>
@@ -55,56 +52,49 @@ export default function TodoList() {
                         <ListItemButton sx={{ pl: 4 }}>
                             <ListItemText primary="My todos" />
                         </ListItemButton>
+
                         {/* Add todo */}
-
                         <ListItemButton sx={{ pl: 4 }}>
-
                             <Link underline="none" href="/addTodo"><ListItemText primary="Add Todo" /></Link>
                         </ListItemButton>
-
                     </List>
                 </Collapse>
             </List>
-
-            {/* card  */}            
-
-            <Grid container sx={{ backgroundColor: "#EEE" }}
+            {/* card  */}
+            <Grid
+                container
+                sx={{ backgroundColor: "#EEE" }}
                 spacing={3}>
-
-
                 {
                     todos.map((todo, index) => (
-
-
-                        <Grid item xs={1} md={3} xl={4}   >
-                            <Card sx={{ maxWidth: 500 }}>
+                        <Grid item
+                            xs={1}
+                            md={3}
+                            xl={4} >
+                            <Card
+                                sx={{ maxWidth: 500 }}>
                                 <CardHeader
                                     avatar={
-                                        <Avatar sx={{ bgcolor: red[500] }}
+                                        <Avatar
+                                            sx={{ bgcolor: red[500] }}
                                             aria-label="Avatar">
-                                            NH
                                         </Avatar>
                                     }
-
                                     action={
                                         <IconButton aria-label="settings">
-                                            <Switch {...label}
-                                                defaultChecked />
-                                        </IconButton>
-                                    }
-                                    title="Label"
-                                />
-                                <Typography padding={2}
+                                            <MoreVertIcon />
+                                        </IconButton>}
+                                    title="Label" />
+
+                                <Typography
+                                    padding={2}
                                     variant="body2"
                                     color="text.secondary">
-                                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                                    {todo.message}
                                 </Typography>
                             </Card>
                         </Grid>
-                    ))
-
-                }
-
+                    ))}
             </Grid>
         </Stack>
     );
